1650037989cf991a9b11651956f33b93f0761107
Phil W <phil.wallace83@gmail.com>
2025-02-20 10:29:39 -0500

    Small fix to camera variables.

--- src/ModelViewerCamera.h
+++ src/ModelViewerCamera.h
@@ -17,7 +17,7 @@ namespace ModelViewer
 		void setPerspectiveProjection(float fovy, float aspect, float near, float far);
 
 		void setViewDirection(glm::vec3 position, glm::vec3 direction, glm::vec3 up = glm::vec3{ 0.0f, -1.0f, 0.0f });
-		void setViewTarget(glm::vec3 target, glm::vec3 direction, glm::vec3 up = glm::vec3{ 0.0f, -1.0f, 0.0f });
+		void setViewTarget(glm::vec3 position, glm::vec3 target, glm::vec3 up = glm::vec3{ 0.0f, -1.0f, 0.0f });
 		void setViewYXZ(glm::vec3 position, glm::vec3 rotation);
 
 		const glm::mat4& getProjection() const { return projectionMatrix; }
